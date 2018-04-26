1b9bed51b6f98c0849e77b227a1081e6d1ce5f24
sahil <sahil@DESKTOP-KGQIGKP.mshome.net>
2018-04-26 22:46:31 +0530

    26-04-2018

--- src/main/java/model/ProductInfo.java
+++ src/main/java/model/ProductInfo.java
@@ -1,5 +1,73 @@
 package model;
 
-public class ProductInfo {
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import entity.Product;
+
+public class ProductInfo {
+	private String code;
+	private String name;
+	private double price;
+	
+	private boolean newProduct = false;
+	
+	private CommonsMultipartFile fileDate;
+	
+	public ProductInfo() {
+		
+	}
+	
+	public ProductInfo(Product product) {
+		this.code = product.getCode();
+		this.name = product.getName();
+		this.price = product.getPrice();
+	}
+	
+	public ProductInfo(String code,String name,double price) {
+		this.code = code;
+		this.name = name;
+		this.price = price;
+		
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public boolean isNewProduct() {
+		return newProduct;
+	}
+
+	public void setNewProduct(boolean newProduct) {
+		this.newProduct = newProduct;
+	}
+
+	public CommonsMultipartFile getFileDate() {
+		return fileDate;
+	}
+
+	public void setFileDate(CommonsMultipartFile fileDate) {
+		this.fileDate = fileDate;
+	}
+	
 }
